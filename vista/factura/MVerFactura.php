db8a5ced27dc981ae30ef9f8616d00b924ca03f3
YoCere <jcerezorios@gmail.com>
2024-09-30 21:11:08 -0400

    Correccion al mostrar el total en la vista factura

--- vista/factura/MVerFactura.php
+++ vista/factura/MVerFactura.php
@@ -78,8 +78,14 @@ $producto=json_decode($factura["detalle"], true);
                 }
                 ?>
                 <tr>
-                    <td colspan="4"><b>Total</b></td>
+                    <td colspan="4"><b>Total Neto</b></td>
                     <td><?php echo $factura["neto"]; ?></td>
+                    </tr>
+                    <td colspan="4"><b>Total Descuento</b></td>
+                    <td><?php echo $factura["descuento"]; ?></td>
+                    </tr>
+                    <td colspan="4"><b>Total</b></td>
+                    <td><?php echo $factura["total"]; ?></td>
                 </tr>
                 </tbody>
 
